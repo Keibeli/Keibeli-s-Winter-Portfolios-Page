5c8a606ba7cb5b707f728240fe39af805f384797
Keibeli <60240854+Keibeli@users.noreply.github.com>
2020-01-23 13:56:55 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("img.picture").click(function() {
+    $(".math").append("<br>functions! ");
+});
+
+$("img.picture").click(function() {
+    $(".coding").append("<br>HTML! CSS! jQuery! ");
+});
+
+$("img.picture").click(function() {
+    $(".history").append("<br>Native Americans! ");
+});
+
+$("img.pictureb").click(function() {
+    $(".english").append("<br>how to write an essay! ");
+});
